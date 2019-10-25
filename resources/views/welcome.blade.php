edccc0ff334d10b4b536ed22dcab3301b4de6247
unknown <13en@protonmail.com>
2019-10-25 15:02:37 +0100

    Updated button label/link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
   <div class="container menu__wrapper">
     <div class="row">
       <div class="menu__logo menu__item">
-        <a href="index.html">
+        <a href="/">
           <svg class="menu__logo-img" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
             <path data-name="Sigma symbol" class="svg-element"
                   d="M237.418,8583.56a12.688,12.688,0,0,0,.419-3.37c-0.036-5.24-2.691-9.68-7.024-13.2h-3.878a20.819,20.819,0,0,1,4.478,13.01c0,4.56-2.456,10.2-6.413,11.4a16.779,16.779,0,0,1-2.236.51c-10.005,1.55-14.109-17.54-9.489-23.31,2.569-3.21,6.206-4.08,11.525-4.08h17.935A24.22,24.22,0,0,1,237.418,8583.56Zm-12.145-24.45c-8.571.02-12.338,0.98-16.061,4.84-6.267,6.49-6.462,20.69,4.754,27.72a24.092,24.092,0,1,1,27.3-32.57h-16v0.01Z"
@@ -66,7 +66,7 @@
       <div class="menu__item">
         <nav class="menu__right-nav d-l-none">
           <ul>
-            <li><a href="10_get-app.html" class="site-btn site-btn--accent">Get the Sigma</a></li>
+            <li><a href="10_get-app.html" class="site-btn site-btn--accent">Dashboard</a></li>
             <li>
               <div class="menu__dropdown d-t-none">
                 <a class="link link--gray menu__dropdown-btn">En
