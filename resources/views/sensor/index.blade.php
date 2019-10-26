e1be06cefe67fc0afd299518b82bbca87a81c735
unknown <13en@protonmail.com>
2019-10-26 17:58:09 +0100

    Adding sensor index table for debugging

--- resources/views/sensor/index.blade.php
+++ resources/views/sensor/index.blade.php
@@ -434,8 +434,6 @@
                     <th>Position</th>
                     <th>Office</th>
                     <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
                   </tr>
                 </thead>
                 <tbody>
