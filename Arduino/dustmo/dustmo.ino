7187cd1e120feb7a27063df76b194459459649a2
unknown <13en@protonmail.com>
2019-10-26 13:27:15 +0100

    modified api

--- Arduino/dustmo/dustmo.ino
+++ Arduino/dustmo/dustmo.ino
@@ -73,7 +73,7 @@ void loop() {
  
   delay(1000);
   
-  String dustDensity, postData;
+  String temperature, humidity, dustDensity, postData;
   voMeasured = analogRead(measurePin); // read the dust value
   calcVoltage = voMeasured * (3.3 / 1024);
   dustDensity = 0.17 * calcVoltage - 0.1;
@@ -88,7 +88,7 @@ void loop() {
   Serial.println(dustDensity);
  
   //prepare request
-  postData = "dust Density:" + dustDensity;
+  postData = "temperature=" + temperature + "dust Density:" + dustDensity + "&humidity=" + humidity;
   http.begin(host);
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpCode = http.POST(postData);
