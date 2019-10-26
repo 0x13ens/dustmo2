e524738a5cade350ce85a84576ae61c523a5896d
unknown <13en@protonmail.com>
2019-10-26 13:30:38 +0100

    modified api

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/', function (\Illuminate\Http\Request $request) {
     \Illuminate\Support\Facades\Storage::append("arduino-log.txt",
         "Time: " . now()->format("Y-m-d H:i:s") . ', ' .
-        "dustDensity: " . $request->get("dustDensity", "n/a") . '°C, ' . '%'
+        "Temperature: " . $request->get("temperature", "n/a") . '°C, ' .
+        "dustDensity: " . $request->get("dustDensity", "n/a") . ' , ' .
+        "Humidity: " . $request->get("humidity", "n/a") . '%'
     );
 });
