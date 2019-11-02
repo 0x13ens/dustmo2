fae75d80137bfa97bedec30d22ef4591ec30c1c4
unknown <13en@protonmail.com>
2019-11-02 15:38:20 +0000

    Updated Sensor

--- resources/views/sensor/index.blade.php
+++ resources/views/sensor/index.blade.php
@@ -55,21 +55,6 @@
             </div>
          </div>
       </div>
-      <div class="col s12 m6 l6 xl3">
-         <div class="card gradient-45deg-green-teal gradient-shadow min-height-100 white-text animate fadeRight">
-            <div class="padding-4">
-               <div class="col s7 m7">
-                  <i class="material-icons background-round mt-5">attach_money</i>
-                  <p>Profit</p>
-               </div>
-               <div class="col s5 m5 right-align">
-                  <h5 class="mb-0 white-text">$890</h5>
-                  <p class="no-margin">Today</p>
-                  <p>$25,000</p>
-               </div>
-            </div>
-         </div>
-      </div>
    </div>
 </div>
 <!--card stats end-->
