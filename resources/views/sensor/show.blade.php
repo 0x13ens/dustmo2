4945672d10b445222f04ef73d986f2238a099484
unknown <13en@protonmail.com>
2019-11-02 18:52:43 +0000

    added indivdial sensor data

--- resources/views/sensor/show.blade.php
+++ resources/views/sensor/show.blade.php
@@ -12,7 +12,7 @@
               <div class="card animate fadeLeft">
                  <div class="card-content  center">
                     <h6 class="card-title font-weight-400 mb-0">Air Nano</h6>
-                    <img src="../../../app-assets/images/cards/airmode-nano.png" alt="" class="responsive-img">
+                    <img src="../../../app-assets/images/cards/airmo-nano.png" alt="" class="responsive-img">
                     <p><b>Bedroom Sensor</b></p>
                     <p>Pressure, Temperature, Humidity</p>
                  </div>
