401e3f7468c898aa970d232edbd236936fcdf18b
unknown <13en@protonmail.com>
2019-11-02 13:32:36 +0000

    added timestamps to sensor table

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -14,7 +14,7 @@ class SensorController extends Controller
      */
     public function index()
     {
-        $sensors = Sensor::orderBy('id', 'DESC')->get()->paginate(50);
+        $sensors = Sensor::orderBy('id', 'DESC')->paginate(50)->get();
         return view('sensor.index', compact('sensors'));
     }
 
