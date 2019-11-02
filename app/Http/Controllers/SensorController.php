ff164dd3e616844737677a38620dbfacaf685ba2
unknown <13en@protonmail.com>
2019-11-02 13:42:29 +0000

    fixed pagination

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -14,7 +14,7 @@ class SensorController extends Controller
      */
     public function index()
     {
-        $sensors = Sensor::orderBy('id', 'DESC')->get();
+        $sensors = Sensor::orderBy('id', 'DESC')->paginate(6);
         return view('sensor.index', compact('sensors'));
     }
 
