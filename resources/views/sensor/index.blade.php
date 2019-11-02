fe9dff95e0e551c65c72f5e749ed713ab5808ef3
unknown <13en@protonmail.com>
2019-11-02 15:39:21 +0000

    Updated Sensor

--- resources/views/sensor/index.blade.php
+++ resources/views/sensor/index.blade.php
@@ -14,7 +14,7 @@
    <div class="row">
       <div class="col s12 m6 l6 xl3">
          <div class="card gradient-45deg-light-blue-cyan gradient-shadow min-height-100 white-text animate fadeLeft">
-            <div class="padding-4">
+            <div class="padding-3">
                <div class="col s7 m7">
                   <i class="material-icons background-round mt-5">add_shopping_cart</i>
                   <p>Pressure</p>
@@ -28,7 +28,7 @@
       </div>
       <div class="col s12 m6 l6 xl3">
          <div class="card gradient-45deg-red-pink gradient-shadow min-height-100 white-text animate fadeLeft">
-            <div class="padding-4">
+            <div class="padding-3">
                <div class="col s7 m7">
                   <i class="material-icons background-round mt-5">perm_identity</i>
                   <p>Feels Like</p>
@@ -42,14 +42,14 @@
       </div>
       <div class="col s12 m6 l6 xl3">
          <div class="card gradient-45deg-amber-amber gradient-shadow min-height-100 white-text animate fadeRight">
-            <div class="padding-4">
+            <div class="padding-3">
                <div class="col s7 m7">
                   <i class="material-icons background-round mt-5">timeline</i>
                   <p>Humidity</p>
                </div>
                <div class="col s5 m5 right-align">
-                  <p class="no-margin"><b>Humidity %</b></p>
-                  <h5 class="mb-0 white-text">690</h5>
+                  <p class="no-margin"><b>Humidity </b></p>
+                  <h5 class="mb-0 white-text">690 %</h5>
                </div>
                </div>
             </div>
