8c55be358445ffda4896d36821e8ef0b8f83f586
unknown <13en@protonmail.com>
2019-11-05 09:33:11 +0000

    updated Sensor Page

--- resources/views/sensor/show.blade.php
+++ resources/views/sensor/show.blade.php
@@ -86,6 +86,50 @@
         </div>
         <!-- ecommerce offers end-->
         <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+        <!-- ecommerce offers start-->
+        <div id="ecommerce-offer">
+                <div class="row">
+                   <div class="col s12 m3">
+                      <div class="card gradient-shadow gradient-45deg-light-blue-cyan border-radius-3 animate fadeUp">
+                         <div class="card-content center">
+                            <img src="../../../app-assets/images/weather/good.png" class="width-40 border-round z-depth-5" alt="">
+                            <h5 class="white-text lighten-4">{{ $temperature->pressure }} <small>hPa</small></h5>
+                            <p class="white-text lighten-4">Pressure</p>
+                         </div>
+                      </div>
+                   </div>
+                   <div class="col s12 m3">
+                      <div class="card gradient-shadow gradient-45deg-red-pink border-radius-3 animate fadeUp">
+                         <div class="card-content center">
+                            <img src="../../../app-assets/images/weather/good.png" class="width-40 border-round z-depth-5" alt="">
+                         <h5 class="white-text lighten-4">{{ $temperature->temperature }} <small>℃</small></h5>
+                            <p class="white-text lighten-4">Temperature</p>
+                         </div>
+                      </div>
+                   </div>
+                   <div class="col s12 m3">
+                      <div class="card gradient-shadow gradient-45deg-amber-amber border-radius-3 animate fadeUp">
+                         <div class="card-content center">
+                            <img src="../../../app-assets/images/weather/good.png" class="width-40 border-round z-depth-5" alt="">
+                            <h5 class="white-text lighten-4">{{ $temperature->altitude }}</h5>
+                            <p class="white-text lighten-4">Altitude</p>
+                         </div>
+                      </div>
+                   </div>
+                   <div class="col s12 m3">
+                      <div class="card gradient-shadow gradient-45deg-green-teal border-radius-3 animate fadeUp">
+                         <div class="card-content center">
+                            <img src="../../../app-assets/images/weather/humidity.png" class="width-40 border-round z-depth-5" alt="">
+                            <h5 class="white-text lighten-4">{{ $temperature->humidity }}  <small>%</small></h5>
+                            <p class="white-text lighten-4">Humidity</p>
+                         </div>
+                      </div>
+                   </div>
+                </div>
+             </div>
+             <!-- ecommerce offers end-->
+             <!-- //////////////////////////////////////////////////////////////////////////// -->
      </div>
 
   <!-- Striped Table -->
