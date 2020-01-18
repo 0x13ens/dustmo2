0c69f1c7da0b8693f93d143182f9ccbea2541361
Ben <core@bens-mbp.home>
2020-01-18 14:39:59 +0000

    Configured the air quality sensor and cleaned up old files

--- Arduino/dustmo/dustmo.ino
+++ Arduino/dustmo/dustmo.ino
@@ -1,87 +1,132 @@
-/*
- * Arduino communication with  a web server example
- */
- 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPClient.h>
+// This is the users wifi creds 
+const char* ssid     = "Lots of Security";
+const char* password = "Mercedes19";
 
-int measurePin = A0;
-int ledPower = 12;
- 
-int samplingTime = 280;
-int deltaTime = 40;
-int sleepTime = 9680;
- 
-float voMeasured = 0;
-float calcVoltage = 0;
-float dustDensity = 0;
- 
-const char *ssid = "Lots of Security";  
-const char *password = "Mercedes19";
- 
-//Web/Server address to read/write from 
-const char *host = "http://dustmo.com/api";   //your IP/web server address
+
+//const char* ssid     = "iPhone";
+//const char* password = "dustmo";
+
+const char* serverName = "http://dustmo.com/api";
+
+#ifdef ESP32
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define I2C_SDA 33
+#define I2C_SCL 32
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+TwoWire I2CBME = TwoWire(0);
+Adafruit_BME280 bme;
+
+unsigned long delayTime;
 
 void setup() {
-  delay(1000);
   Serial.begin(115200);
-  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
-  delay(1000);
-  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
-  
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
- 
-  Serial.print("Connecting");
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.println(F("BME280 test"));
+  I2CBME.begin(I2C_SDA, I2C_SCL, 100000);
+
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76, &I2CBME);  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+
+  Serial.println();
+  delay(4000);   //Delay needed before calling the WiFi.begin
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
- 
-  //If connection successful show IP address in serial monitor
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
 }
- 
+
 void loop() {
-  //Declare object of class HTTPClient
-  HTTPClient http;
 
-  //Prepare data
+  printValues();
+  delay(delayTime);
 
- 
-  delay(1000);
+ //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
 
-  String postData;
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
 
-    voMeasured = analogRead(measurePin); // read the dust value
-    calcVoltage = voMeasured * (3.3 / 1024);
-    dustDensity = 0.17 * calcVoltage - 0.1;
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String httpRequestData =
+    "&temperature=" + String(bme.readTemperature()) +
+    "&pressure=" + (bme.readPressure() / 100.0F) +
+    "&altitude=" + (bme.readAltitude(SEALEVELPRESSURE_HPA)) +
+    "&humidity=" + (bme.readHumidity());
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
 
-  Serial.print("Raw Signal Value (0-1023): ");
-  Serial.println(voMeasured);
- 
-  Serial.print(" - Voltage: ");
-  Serial.println(calcVoltage);
- 
-  Serial.print(" - Dust Density: ");
-  Serial.println(dustDensity);
- 
-  //prepare request
-  postData = "&voMeasured=" + String(voMeasured) + "&calcVoltage=" + calcVoltage + "&dustDensity=" + dustDensity;
-  http.begin(host);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int httpCode = http.POST(postData);
-  String payload = http.getString();
- 
-  Serial.println(httpCode);
-  Serial.println(payload);
-  http.end();
-  delay(5000); //save to the database every 5 seconds
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(10000);
+}
+
+void printValues() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
 }
