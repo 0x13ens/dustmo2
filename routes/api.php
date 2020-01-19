f01d0a3487b6e5b7e901c74ada81006f858227e4
Ben <core@bens-mbp.home>
2020-01-19 10:44:10 +0000

    configured register api

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v1')->group(function(){
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+    Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('getUser', 'Api\AuthController@getUser');
+    });
+});
+
 // This post link hits the sensorController Store method and hopefully stores our sensor values in the database
 Route::post('/','SensorController@store');
+
