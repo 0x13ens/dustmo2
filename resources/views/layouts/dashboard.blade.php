569e15400770703746bc5718b0cd98ebc0ef2086
Ben <core@bens-mbp.home>
2020-01-20 23:55:59 +0000

    Logout working theme issue / read comment

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -77,9 +77,9 @@
               <li class="divider"></li>
               <li><a class="grey-text text-darken-1" href="user-lock-screen.html"><i class="material-icons">lock_outline</i> Lock</a></li>
               <!-- TODO: Logout Displays white space? -->
-              <li><a class="grey-text text-darken-1" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+              <a class="grey-text text-darken-1" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                 <i class="material-icons">keyboard_tab</i> Logout</a>
-            </a><li>
+            </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
