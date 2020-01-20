89e62502d89e23b092898145ada6a105e70a89d3
Ben <core@bens-mbp.home>
2020-01-20 21:25:05 +0000

    Redirect users straight to dashboard upon registering

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,19 +1,35 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 class ExampleTest extends TestCase
+
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
-    }
+
+   /**
+
+    * A basic test example.
+
+    *
+
+    * @return void
+
+    */
+
+   public function testBasicTest()
+
+   {
+
+       $response = $this->get('/');
+
+       $response->assertStatus(200);
+
+   }
+
 }
