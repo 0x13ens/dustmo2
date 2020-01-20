a9b09af2bf2604ee7f5d402ff20bf78cb30cd2d2
Ben <core@bens-mbp.home>
2020-01-20 21:25:56 +0000

    Redirect users to sensors index

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/sensors';
 
     /**
      * Create a new controller instance.
