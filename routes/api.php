485993651a38e3949ca023c7bb494d0532a8062c
Ben <core@bens-mbp.home>
2020-02-03 00:48:06 +0000

    apiController

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,15 @@ Route::post('sensors','SensorController@store');
 Route::get('sensors', 'SensorController@getSensors');
 
 // This post link hits the sensorController Store method and creates a user in our database
-Route::post('login', 'API\RegisterController@login');
 Route::post('createuser', 'UserController@create');
 
 // This post link hits the UserController login method and hopefully logs our user in
 Route::get('userlogin', 'UserController@login');
+
+Route::prefix('v1')->group(function(){
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('register', 'Api\AuthController@register');
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('getUser', 'Api\AuthController@getUser');
+    });
+});
